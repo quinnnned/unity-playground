0a4398cb8a7dd4950c4d2bac51d2abee497cf180
JavaScriptSandbox <JesseDRobertson@gmail.com>
2017-05-16 20:21:51 -0500

    initial

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public Text output;
+
+    void Start() {
+
+        string[] giantspeak = { "fee", "fi", "fo", "fum" };
+
+        this.output.text = ""
+           + Sampler.Sample(giantspeak) + "\n"
+           + Sampler.Sample(giantspeak) + "\n"
+           + Sampler.Sample(giantspeak) + "\n"
+           + Sampler.Sample(giantspeak) + "\n"
+           + Sampler.Sample(giantspeak) + "\n"
+           + Sampler.Sample(giantspeak) + "\n";
+    }
+}
---
+++ Assets/Sampler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sampler {
+
+    public static T Sample<T>(T[] things) {
+        int length = things.Length;
+        int index = Mathf.FloorToInt(length * 100 * Random.value) % length;
+        return things[index];
+    }
+}
